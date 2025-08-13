92f2a83ff2770d83db2a56a1850568d3e55ea344
Vincent Llapitan <vincent@labtestingapi.com>
2025-08-13 10:21:46 -0400

    add api key

--- infra/modules/api/outputs.tf
+++ infra/modules/api/outputs.tf
@@ -1,6 +1,10 @@
 output "api_base_url" {
   value = aws_apigatewayv2_stage.prod.invoke_url
 }
+output "api_key_value" {
+  value = aws_api_gateway_api_key.this.value
+  sensitive = true
+}
 output "api_id" {
   value = aws_apigatewayv2_api.todos.id
 }
